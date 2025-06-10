f462d52d9db0ffdfe5fd454b047e5f34c48fd330
KKamJi <xowl5460@naver.com>
2025-06-10 22:40:22 +0900

    fix: update cluster name in setup-kubeconfig.sh

--- app-of-apps-basic/setup-kubeconfig.sh
+++ app-of-apps-basic/setup-kubeconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws eks update-kubeconfig --region ap-northeast-2 --name kkamji-al2023-prod --alias kkamji-al2023-prod
+aws eks update-kubeconfig --region ap-northeast-2 --name kkamji-al2023 --alias kkamji-al2023
 argocd login argocd.kkamji.net --username admin --grpc-web
-argocd cluster add kkamji-al2023-prod --name kkamji-al2023-prod
+argocd cluster add kkamji-al2023 --name kkamji-al2023
